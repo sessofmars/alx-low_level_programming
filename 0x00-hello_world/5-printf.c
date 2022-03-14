c709d5a3193efc3feae58615ef7ff1979b641974
sessofmars <sesughnanakaan@gmail.com>
2022-03-14 00:18:56 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - entry point
-* return 0 if exited properl, non-zero otherwise
+* Main - Entry point
+* Return - Always 0 if successful
 */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art");
-	return 0;
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
 }
