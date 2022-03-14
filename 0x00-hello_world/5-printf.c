bf267564fc1426bd5f2d0c9911bf2f2bf786a970
sessofmars <sesughnanakaan@gmail.com>
2022-03-13 18:34:53 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-#include <string.h>
-int main()
+/**
+* main - entry point
+* return 0 if exited properl, non-zero otherwise
+*/
+int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art");
 	return 0;
