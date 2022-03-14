3ecafb401a8a4ea2474d501a403ba4b5bbd318c2
sessofmars <sesughnanakaan@gmail.com>
2022-03-14 00:57:58 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* Main - Entry point 
+* main - Entry point 
 * Return - Always 0 if successful
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,");
+	Return (0);
 }
