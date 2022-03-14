58e4cf1477bdbeb42ac59c38bd326a8bc30c375d
sessofmars <sesughnanakaan@gmail.com>
2022-03-14 01:34:15 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
 * main - entry point
+* description - print script
 * Return - Always 0 if successful
 */
-int main()
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
