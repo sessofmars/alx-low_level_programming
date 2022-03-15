50d2f5c241971aa8264d1d5656091c507c395c55
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-03-16 00:53:48 +0100

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include main.h
+#include "main.h"
 #include <unistd.h>
 
 /**
@@ -7,8 +7,16 @@
  * Return: 0 (Success)
  */
 
-int main(void){
-	int write("_putchar",\n);
-
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	
 	return 0;
 }
