8f5b08c458bde6ace4a47a42b043db14cfc19174
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-03-16 02:26:42 +0100

    10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_10x - prints alphabets 10 times
+ * Description: Function that prints ten times the alphabet 
+ * followed by a new line
+ */
+
+void print_alphabet_10x(void)
+{
+	int i, j;
+
+	for (i=0; i < 10; i++)
+	{
+		for (j = 'a'; j<= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
