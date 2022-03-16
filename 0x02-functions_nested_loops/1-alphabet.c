c142a1fb4df0a2d554ec1d210cf1f3484440ed67
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-03-16 02:08:05 +0100

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets, in lowercase, followed bya new line
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	
+	_putchar('\n');
+}
