e7bf3e1701f074bcccb375f2f89f8728f201e210
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-03-21 01:44:36 +0100

    sensei

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print - numbers - print numbers between 0 to 9.
+ * Return: no return.
+ */
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = 48; ch < 58; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
