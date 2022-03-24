15a1b2047a5977644d5aa00e53f0d42871c557bb
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-03-24 15:13:11 +0100

    ll

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,15 +12,15 @@ char *_strcat(char *dest, char *src);
 {
 	int i, c;
 
-	for (i = 0; dest[i] != '\0'; i++)
-		;
+	for (i = 0; dest[i] != '\0'; i++);
 
-	for (c = 0; src[c] != '\0'; c++)
+	for (c = 0; src[c] != '\0'; c++);
 	{
 		dest[i] = src[c];
 		i++;
 	}
 
 	dest[i] = '\0';
+
 	return (dest);
 }
