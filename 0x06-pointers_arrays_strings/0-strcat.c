a6664c80d006f509498ba3032d78e4505926dee9
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-03-24 01:06:08 +0100

    life

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,14 +7,29 @@
  * @s2: second character.
  *
  * Return: (void)
+ *
  */
+
 
 char *_strcat(char *dest, char *src)
 {
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    strcat(s1,s2);
-    putchar("s1,s2: %s\n", s1);
+    char str1[MAX_SIZE], str2[MAX_SIZE];
+    char * s1 = str1;
+    char * s2 = str2;
+
+    /* Input two strings from user */
+    printf("Enter first string: ");
+    gets(str1);
+    printf("Enter second string: ");
+    gets(str2);
+
+    /* Move till the end of str1 */
+    while(*(++s1));
+
+    /* Copy str2 to str1 */
+    while(*(s1++) = *(s2++));
+
+    printf("Concatenated string = %s", str1);
 
     return 0;
 }
