04ae8b89d12bd5eca2cfbeb7ef5ec82d09502b2e
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-03-29 17:50:49 +0100

    move

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _memset - fills memory with a constant byte,
@@ -14,7 +13,6 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		*(s + i) =  b;
-
+		*s[i] =  b;
 	return (s);
 }
