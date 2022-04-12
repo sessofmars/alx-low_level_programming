8f4fdd02f87dbadd6b14240a11a447769f591bf2
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-04-12 19:55:33 +0100

    a good week

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef _SUM_LIKE_MACRO_
+#define _SUM_LIKE_MACRO_
+
+/**
+ * main - Write a macro like function that computes
+ * the sum of the numbers x and y.
+ * 
+ * Return: Always 0.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
