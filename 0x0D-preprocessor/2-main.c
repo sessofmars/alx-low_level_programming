074a68e18e042d85c044b8b57d11c86b0f20a4bd
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-04-12 18:25:19 +0100

    real

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,9 +3,11 @@
 /**
  * main - a program that prints the name of the file it was compiled from
  * followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	printf("filename.\n");
-	return 0;
+	printf("%s\n", __FILE__);
+	return (0);
 }
