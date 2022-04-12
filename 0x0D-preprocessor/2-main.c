009bc88a28470e94f1fdaddfce224b3d0ea6b55b
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-04-12 18:21:23 +0100

    abundance

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+/**
+ * main - a program that prints the name of the file it was compiled from
+ * followed by a new line.
+ */
+int main(void)
+{
+	printf("filename.\n");
+	return 0;
+}
