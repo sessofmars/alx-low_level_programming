37b2fb4bdd16858e934ac4f4a70abb557fd2c3a3
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-04-15 02:23:06 +0100

    the sweetest dreams

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * main - a function that returns the sum of all it's parameters.
+ *
+ * return: 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist; //holds information needed by other macros.
+	unsigned int i; //declaration of int i
+	int sum = 0; //initialisation
+
+	if (n == 0) //conditional statement
+		return (0);
+
+	va_start(valist, n); //access to variadic function enabled
+
+	for (i = 0; i < n; i++) //for loop
+		sum += va_arg(valist, int); //access to next variadic function enabled.
+
+	va_end(valist); //ends traversal of variadic function
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,2 @@
+0x10-variadic_functions: tasks 0 - 3
+
