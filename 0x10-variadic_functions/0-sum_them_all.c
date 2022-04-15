e6b3a074c88fb2836a07b588f0dc4e60efa8b7f6
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-04-15 02:45:19 +0100

    obsessed with winning

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,11 +1,10 @@
-#include <variadic_functions.h>
+#include "variadic_functions.h"
 
 /**
- * main - a function that returns the sum of all it's parameters.
+ * sum_them_all - returns the sum of all its parameters.
+ * @n: amount of the arguments.
  *
- *@n: amount of the arguments
- *
- * return: sum of its parameters
+ * Return: sum of its parameters.
  */
 int sum_them_all(const unsigned int n, ...)
 {
