7f3a2c3d4f7c7adc2aaf8db39a9a4e6e320bd813
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-04-16 06:48:03 +0100

    hit that

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "functions_pointers.h"
+
+/**
+ * main - a function that prints a name.
+ * @name: name of the person.
+ * @f: function pointer.
+ *
+ * Return: a name.
+ */
+
+void print_name(char *name, void (*f)(char *));
+{
+	if(name && f)
+		f(name);
+}
