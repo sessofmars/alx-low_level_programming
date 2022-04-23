dcf85679351d1d4f3a8223979755fafe921252db
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-04-23 08:38:55 +0100

    mandem

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,36 +1,29 @@
 #include "lists.h"
-
 /**
- * add_node - adds new node at the beginning of a linked list
- * @head: pointer to the head node
- * @str: pointer to string to be duplicated
- * Return: pointer to new element
-*/
+ * add_node - adds a new node at the beginning
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
+ */
+
 list_t *add_node(list_t **head, const char *str)
 {
-	int length;
+	list_t *new;
+	size_t nchar;
 
-	list_t *new_node = malloc(sizeof(list_t));
-
-	if (new_node == NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+	new->str = strdup(str);
 
-	length = 0;
-	while (new_node->str[length])
-		length++;
+	for (nchar = 0; str[nchar]; nchar++)
+		;
 
-	new_node->len = length;
+	new->len = nchar;
+	new->next = *head;
+	*head = new;
 
-	new_node->next = (*head);
-
-	(*head) = new_node;
-
-	return (new_node);
+	return (*head);
 }
