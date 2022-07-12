a8e2fae2c3f0be7d97afa9d8f60a253fd85e3916
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-07-12 04:56:24 +0100

    adds elements to hashtable

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,54 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table.
+ *
+ * @ht: the hash table
+ * @key: the key
+ * @value: the value associated with the key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *hash_node;
+	char *value_copy;
+	unsigned long int index, i;
+
+	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
+		return (0);
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
+
+	hash_node = malloc(sizeof(hash_node_t));
+	if (hash_node == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	hash_node->key = strdup(key);
+	if (hash_node->key == NULL)
+	{
+		free(hash_node);
+		return (0);
+	}
+	hash_node->value = value_copy;
+	hash_node->next = ht->array[index];
+	ht->array[index] = hash_node;
+
+	return (1);
+}
